8e5e03dc135e1aaf3638af17276887754b3b829b
uzulla <uzulla@himitsukichi.com>
2023-06-22 11:22:55 +0900

    update app.

--- html/index.php
+++ html/index.php
@@ -15,16 +15,33 @@
 //echo $wrong_var["missing"];
 //throw new Exception(''); // 500
 
-$name = $_GET['name'] ?? " John doe";
+// escape echo function
+$ee = function (string $str) {
+    echo htmlspecialchars(
+        $str . PHP_EOL,
+        ENT_QUOTES,
+        'UTF-8'
+    );
+};
+
+// parse URI
+$path = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+
+// Old school App
+if (preg_match('#\A/hello/([a-z]+)\z#u', $path, $matches)) {
+    $ee("hello {$matches[1]}");
+
+} elseif (preg_match('#\A/clock\z#u', $path)) {
+    $dt = new DateTimeImmutable("now", new DateTimeZone('Asia/Tokyo'));
+    $ee($dt->format('Y-m-d H:i:s'));
+
+} else {
+    $ee("index page");
+
+}
 
 //some dump sample.
 //file_put_contents('dump', serialize($_GET));
 //$_GET = unserialize(file_get_contents('dump'));
 
-$dt = new DateTimeImmutable("now", new DateTimeZone('Asia/Tokyo'));
-echo htmlspecialchars(
-    "hello {$name}, it's {$dt->format('Y-m-d H:i:s')}" . PHP_EOL,
-    ENT_QUOTES,
-    'UTF-8'
-);
 
